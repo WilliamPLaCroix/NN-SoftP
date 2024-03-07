1730577b2f7acfe92c6bc07bb6f6f7209888e564
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-07 14:45:24 +0100

    reduce batches

--- trying_BERT.py
+++ trying_BERT.py
@@ -130,7 +130,7 @@ def main():
         predictions = []
         targets = []
         for batch_number, batch in enumerate(val_dataloader):
-            if batch_number > 10:
+            if batch_number > 2:
                 return
             batch.to(device)
     
