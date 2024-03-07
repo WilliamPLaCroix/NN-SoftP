ad12b391b885e50ae3e39dda007384b339c71de5
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-07 16:00:53 +0100

    reduce batch size

--- trying_BERT.py
+++ trying_BERT.py
@@ -100,7 +100,7 @@ def main():
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
     
-    batch_size = 128
+    batch_size = 32
     learning_rate = 0.01
 
     API_TOKEN = "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"
