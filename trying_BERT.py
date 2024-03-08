a75bdc11b5baa741047d252ff6e3d756c3c07afe
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-08 15:14:31 +0100

    oops

--- trying_BERT.py
+++ trying_BERT.py
@@ -52,7 +52,8 @@ class Classifier(torch.nn.Module):
         #print("subword_surp", subword_surp.shape, subword_surp.dtype)
         #print("subword_surp", subword_surp.shape, subword_surp.dtype)
         mean_surprisal = subword_surp.sum(dim=1) / attention_mask.sum(dim=1)
-        print(mean_surprisal.item())
+        print(mean_surprisal[0].item())
+        return
         #print("mean_surprisal", mean_surprisal.shape, mean_surprisal.dtype)
         outputs = torch.mean(outputs, dim=1, dtype=bnb_config.bnb_4bit_compute_dtype)
         outputs = self.classifier(torch.cat((outputs, 
