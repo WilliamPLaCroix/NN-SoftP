ece26cabac42ed5c07d32463bf4700e2df9c11d7
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-08 14:05:46 +0100

    reduce lstm layers

--- trying_BERT.py
+++ trying_BERT.py
@@ -22,7 +22,7 @@ class Classifier(torch.nn.Module):
         self.intermediate_size = 10
         self.hidden_size = 100
         self.lstm = torch.nn.LSTM(input_size=self.lm_out_size, hidden_size=self.hidden_size, 
-                                  num_layers=2, batch_first=True, bidirectional=False, dtype=bnb_config.bnb_4bit_compute_dtype)#, proj_size=self.proj_size,)
+                                  num_layers=1, batch_first=True, bidirectional=False, dtype=bnb_config.bnb_4bit_compute_dtype)#, proj_size=self.proj_size,)
         self.lstm_classifier = torch.nn.Linear(self.hidden_size+4, num_classes, dtype=bnb_config.bnb_4bit_compute_dtype)
         self.activation = torch.nn.Sigmoid()
         self.batch_norm = torch.nn.BatchNorm1d(self.lm_out_size, dtype=bnb_config.bnb_4bit_compute_dtype)
