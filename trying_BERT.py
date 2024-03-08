0d55ff76c8c67e25a9e1ebbb1d72d7cfef33cd7e
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-08 14:09:59 +0100

    memory allocation

--- trying_BERT.py
+++ trying_BERT.py
@@ -113,7 +113,7 @@ def main():
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
     
-    batch_size = 32
+    batch_size = 16
     learning_rate = 0.0001
 
     API_TOKEN = "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"
