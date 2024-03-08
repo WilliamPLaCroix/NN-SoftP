74dcc811c931e3bc2e139d9c89ff323b9032f5ce
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-08 10:14:56 +0100

    return loss

--- trying_BERT.py
+++ trying_BERT.py
@@ -39,11 +39,7 @@ class Classifier(torch.nn.Module):
         print("input_ids", input_ids.shape, input_ids.dtype)
         # dummy forward pass, not real architecture
         outputs = self.lm(input_ids, attention_mask, output_hidden_states=True, labels=input_ids)
-        print("loss", outputs, outputs.loss)
-        print(outputs.logits)
-        print("loss", outputs.loss["logits"].shape, outputs.loss["logits"].dtype)
-        word_logits = outputs.loss["logits"][:,input_ids[:,]]
-        print("word_logits", word_logits.shape, word_logits.dtype)
+        print("loss", outputs.loss.shape, outputs.loss)
         surprisal = torch.mean(outputs.loss, dim=1, dtype=bnb_config.bnb_4bit_compute_dtype)
         # print("lm output", outputs.shape, outputs.dtype)
         # print("outputs", outputs)
