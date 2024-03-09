c877c2f5851f8677dbd1502b4566b09ab0957d08
wolfingten <gurtler.jakob@gmail.com>
2024-03-09 16:02:26 +0100

    add wandb key path

--- xlm_roberta_finetune.py
+++ xlm_roberta_finetune.py
@@ -20,7 +20,7 @@ WANDB_PATH = "/data/users/jguertler/.cache/wandb.tok"
 with open(WANDB_PATH, "r", encoding="UTF-8") as f:
     wandb_tok = f.read().strip()
 
-wandb.login(wandb_tok)
+wandb.login(key=wandb_tok)
 
 EPOCHS = 10
 BATCH_SIZE = 16
@@ -57,7 +57,14 @@ data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 model = XLMRobertaForSequenceClassification.from_pretrained(
     CHECKPOINT,
     num_labels=NUM_LABELS,
-    classifier_dropout=0.1)
+    classifier_dropout=0.1)# set the wandb project where this run will be logged
+os.environ["WANDB_PROJECT"]="my-awesome-project"
+
+# save your trained model checkpoint to wandb
+os.environ["WANDB_LOG_MODEL"]="true"
+
+# turn off watch to log faster
+os.environ["WANDB_WATCH"]="false"
 
 
 def compute_metrics(pred):
