a0a9477787c8326ebec331b8d76a6359518c9ccf
pj <phha00002@stud.uni-saarland.de>
2024-03-14 23:12:27 +0100

    set 1000 years (epochs)

--- cluster_templates/better_BERT_SimpleLinear1.py
+++ cluster_templates/better_BERT_SimpleLinear1.py
@@ -42,7 +42,7 @@ experiment = {
     "CLF_HEAD" : "SimplestLinearHead", # not used in code, define yourself
     "FREEZE_LM" : True, # USED
     "BATCH_SIZE" : 32, # USED
-    "NUM_EPOCHS" : 100, # USED
+    "NUM_EPOCHS" : 1000, # USED
     "EARLY_STOPPING_AFTER" : "NEVER", # USED
     "LEARNING_RATE" : 0.001, # USED
     "OPTIMIZER" : "Adam", # not used in code, define yourself
