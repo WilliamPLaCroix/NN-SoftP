2e2cabe69810405622303c668e7b0258b7126f22
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-15 13:03:29 +0100

    remove temp statements

--- cluster_templates/helloworld.py
+++ cluster_templates/helloworld.py
@@ -1,6 +1,4 @@
 import sys
-temp = sys.stdout
 sys.stdout = open('/nethome/wplacroix/NN-SoftP/cluster_templates/helloworld.log', 'w')
-sys.stdout.write("hello world!")
+print("hello world2!")
 sys.stdout.close()
-sys.stdout = temp
