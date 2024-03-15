f9c72b2a260aad4d474d919b433928a414527ea1
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-15 13:07:11 +0100

    add directory

--- cluster_templates/helloworld.py
+++ cluster_templates/helloworld.py
@@ -1,4 +1,8 @@
 import sys
-sys.stdout = open('/nethome/wplacroix/NN-SoftP/cluster_templates/helloworld.log', 'w')
+import os
+directory = "/nethome/wplacroix/NN-SoftP/cluster_templates/hello_world/"
+os.mkdir(directory)
+experiment_name = "helloworld"
+sys.stdout = open(f'{directory}{experiment_name}.log', 'w')
 print("hello world2!")
 sys.stdout.close()
