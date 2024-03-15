130d6cf92efbf58f9d4552eaf4932d02f9275598
WilliamPLaCroix <williamplacroix@gmail.com>
2024-03-15 13:31:15 +0100

    update driectory path

--- cluster_templates/helloworld.py
+++ cluster_templates/helloworld.py
@@ -1,6 +1,6 @@
 import sys
 import os
-directory = "/nethome/wplacroix/NN-SoftP/cluster_templates/hello_world/"
+directory = "/data/users/wplacroix/logs/hello_world/"
 os.mkdir(directory)
 experiment_name = "helloworld"
 sys.stdout = open(f'{directory}{experiment_name}.log', 'w')
