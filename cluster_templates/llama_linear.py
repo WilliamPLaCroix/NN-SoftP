6e1967fce192d8c04b8a9704ec5cbd6e48382643
wolfingten <gurtler.jakob@gmail.com>
2024-03-18 15:02:19 +0100

    schwift with me

--- cluster_templates/llama_linear.py
+++ cluster_templates/llama_linear.py
@@ -338,7 +338,7 @@ try:
             classifier_outputs = classifier_outputs.view(-1).to(torch.float64)
             print(classifier_outputs)
             print(batch["labels"])
-            loss_fn = nn.BCEWithLogitsLoss(pos_weight=binary_weight)
+            loss_fn = nn.BCEWithLogitsLoss()#pos_weight=binary_weight)
             loss = loss_fn(classifier_outputs, batch["labels"])
             train_losses.append(loss.item())
 
